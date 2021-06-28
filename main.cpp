24634b3c4b2c00b99aea1a091dea118d13aa0a17
MarcosAlves <82622925+marcosouza97@users.noreply.github.com>
2021-06-28 17:12:58 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <locale.h>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+  int opção=0;
+  ofstream gravar;
+  ifstream ler;
+
+
+  // Solicitando a opção para o usuario
+  cout <<"qual opção você deseja:" << endl;
+  cout << "1 - gravar"<< endl;
+  cout << "2 - ler" << endl;
+  cin >> opção;
+
+  if (opção == 2){
+
+
+  }
+}
